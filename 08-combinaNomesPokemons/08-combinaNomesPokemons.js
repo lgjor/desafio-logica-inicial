879a7ab9c8e3b6e64a1307b1e6fe9f176e8d4c1c
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-05-13 18:47:09 -0300

    Adicionado desafio combinar nomes pokemons

---
+++ 08-combinaNomesPokemons/08-combinaNomesPokemons.js
@@ -0,0 +1,12 @@
+function combinandoNomesPokemons(nomeEntrada){
+  return nomeEntrada+"saur";
+}
+
+// Entrada da palavra usando o gets():
+var nomeEntrada = Buba;//gets();
+
+// Chamando a função "combinandoNomesPokemons" com o nome pokemon informado e armazenando o resultado na variável "palavraGerada":
+var palavraGerada = combinandoNomesPokemons(nomeEntrada);
+
+// Exibindo a palavra gerada:
+console.log(palavraGerada);
